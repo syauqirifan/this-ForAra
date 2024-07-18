744c65cc7c95053f23611a8e3c6495b2f6c2db50
syauqirifan <syauqirifan73@gmail.com>
2024-07-19 02:16:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSideBar() {
+    const sideBar = document.querySelector('.sideBar') 
+    sideBar.style.width = '250px'      
+}
+function hideSideBar() {
+    const sideBar = document.querySelector('.sideBar') 
+    sideBar.style.width = '0px'    
+} 
